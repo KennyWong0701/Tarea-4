05090b5c658dc76da60a4b3649430edd6355b995
Kenny R. Wong <99190650+KennyWong0701@users.noreply.github.com>
2022-06-08 10:46:37 -0600

    Add files via upload

---
+++ string.js
@@ -0,0 +1,29 @@
+//string .slice(): Lo que hace es que extrae el texto de una cadena desde un pocisión hasta otra
+
+var taller = "Esto es un taller de React";
+var ejemplo1 = taller.slice(5,-5);
+
+//console.log(ejemplo1);
+
+//Puntos importantes
+//Números positivos o van a seer incluidos en el nuevo string
+//Se puede utilizar solo un número positivo o negativo
+//Numeros negativos van a ser incluidos en el nuevo string
+//Se pueden utilizar dos numeros negativos
+
+
+
+//sting .trim(): Elimina espacios en blanco al inicio y al final de la cadena
+
+var espacios = "   Trim   "
+//console.log(espacios.trim());
+
+//string .trimEnd(): Al igual que el anterior pero solo elimina los espacios al final
+
+//console.log(espacios.trimEnd());
+
+
+//string .trimStart(): Al igual que el anterior pero solo elimina los espacios al inicio
+
+//console.log(espacios.trimStart());
+
